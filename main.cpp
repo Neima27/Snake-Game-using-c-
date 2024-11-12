09ead0567b3fba2290e888545434225f9cca0582
SIFEN317 <sifenabduselam7@gmail.com>
2024-11-11 23:58:43 -0800

    Update main.cpp
    Implemented user input handling for game controls. Allows the player to control direction (left, right, up, down) and quit the game with the 'x' key.

--- main.cpp
+++ main.cpp
@@ -45,3 +45,29 @@ void GameInit()
 }
 
 // Function for creating the game board & rendering
+// Function to handle user UserInput
+void UserInput()
+{
+    // Checks if a key is pressed or not
+    if (_kbhit()) {
+        // Getting the pressed key
+        switch (_getch()) {
+        case 'a':
+            sDir = LEFT;
+            break;
+        case 'd':
+            sDir = RIGHT;
+            break;
+        case 'w':
+            sDir = UP;
+            break;
+        case 's':
+            sDir = DOWN;
+            break;
+        case 'x':
+            isGameOver = true;
+            break;
+        }
+    }
+}
+
