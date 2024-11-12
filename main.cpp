57fc843460528c4d22a70d77542aa299dc775835
Neima27 <neimamohammedhussen1234@gmail.com>
2024-11-12 06:08:01 -0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -43,6 +43,57 @@ void GameInit()
     fruitCordY = rand() % height;
     playerScore = 0;
 }
+// Function for updating the game state
+void UpdateGame()
+{
+    int prevX = snakeTailX[0];
+    int prevY = snakeTailY[0];
+    int prev2X, prev2Y;
+    snakeTailX[0] = x;
+    snakeTailY[0] = y;
+
+    for (int i = 1; i < snakeTailLen; i++) {
+        prev2X = snakeTailX[i];
+        prev2Y = snakeTailY[i];
+        snakeTailX[i] = prevX;
+        snakeTailY[i] = prevY;
+        prevX = prev2X;
+        prevY = prev2Y;
+    }
+
+    switch (sDir) {
+    case LEFT:
+        x--;
+        break;
+    case RIGHT:
+        x++;
+        break;
+    case UP:
+        y--;
+        break;
+    case DOWN:
+        y++;
+        break;
+    }
+
+    // Checks for snake's collision with the wall (|)
+    if (x >= width || x < 0 || y >= height || y < 0)
+        isGameOver = true;
+
+    // Checks for collision with the tail (o)
+    for (int i = 0; i < snakeTailLen; i++) {
+        if (snakeTailX[i] == x && snakeTailY[i] == y)
+            isGameOver = true;
+    }
+
+    // Checks for snake's collision with the food (#)
+    if (x == fruitCordX && y == fruitCordY) {
+        playerScore += 10;
+        fruitCordX = rand() % width;
+        fruitCordY = rand() % height;
+        snakeTailLen++;
+    }
+}
 
 // Function to set the game difficulty level
 int SetDifficulty()
